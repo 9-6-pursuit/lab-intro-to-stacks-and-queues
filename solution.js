e7610902e0a772ee5a9856caddaf0a332a1b8a5d
Raymond <raymondjohnson@pursuit.org>
2023-09-27 22:26:56 -0400

    in progress

--- solution.js
+++ solution.js
@@ -1,8 +1,56 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
+console.log("Connected!!")
+class Node {
+  constructor(data){
+    this.top = data
+    this.next = null
+  }
+}
+class Stack {
+  constructor(data){
+    this.top = null
+  }
+
+  push(data){
+    let newNode = new Node(data)
+    newNode.next = this.top
+    this.top = newNode
+  }
+
+  size(){
+    let currentSize = 0
+    let currentNode = this.top
+    while(currentNode){
+      currentSize += 1
+    }
+    return currentSize
+  }
+  
+  pop(){
+
+  }
+  
+  isEmpty(){
+
+  }
+  
+  findMin(){
+
+  }
+  
+  peek(){
+
+  }
+  
+}
+
+
+
+
 
 module.exports = {
   Node,
-  Queue,
+  //Queue,
   Stack,
 };
